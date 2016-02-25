c10b81b03066b364aa5bb91e40a1330c31732d09
sion03 <black03bittersweet@gmail.com>
2016-02-25 22:12:20 +0900

    Some Errors remained.

--- app/src/main/java/com/lifeistech/android/bookmark/TAG.java
+++ app/src/main/java/com/lifeistech/android/bookmark/TAG.java
@@ -1,16 +1,24 @@
 package com.lifeistech.android.bookmark;
 
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
 /**
  * Created by sion on 2/25/2016.
  */
 public class TAG {
-    int[] tagA;
-    int[] tagB;
-    int[] tagC;
-    int[] tagD;
+    int[] tagArraya;
+    int[] tagArrayb;
+    int[] tagArrayc;
+    int[] tagArrayd;
 
-    
-
-
+    tagArraya = new int[10];
+    tagArrayb = new int[10];
+    tagArrayc = new int[10];
+    tagArrayd = new int[10];
 
 }
+
+
+
